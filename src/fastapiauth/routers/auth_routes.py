87a1cc19128d0a61467710d950ccf6b5e4a9d2cf
Menelisi Mthembu <menelismthembu@gmail.com>
2026-09-24 02:34:14 +0200

    Refactor

--- src/fastapiauth/routers/auth_routes.py
+++ src/fastapiauth/routers/auth_routes.py
@@ -3,47 +3,43 @@ from sqlalchemy.ext.asyncio import AsyncConnection
 from jwt.exceptions import InvalidTokenError, ExpiredSignatureError
 from datetime import timedelta
 
-from ..security.bearer import JwtBearer
-from ..database.database import get_database
-from ..schemas.user_schemas import User, UserCreate, UserUpdate, TokenResponse, UserLogin
-from ..security.auth import create_access_token, decode_access_token
-from .. import TOKEN_EXPIRE_MINUTES
-from ..repository.user_repository import UserRepository
+from src.fastapiauth.security.bearer import JwtBearer
+from src.fastapiauth.database.database import get_database
+from src.fastapiauth.schemas.user_schemas import User, UserCreate, UserUpdate, TokenResponse, UserLogin
+from src.fastapiauth.security.auth import create_access_token, decode_access_token
+from src.fastapiauth import TOKEN_EXPIRE_MINUTES
+from src.fastapiauth.repository.user_repository import UserRepository
 
 auth_router = APIRouter(
     prefix="/api/v1/auth",
     tags=["auth"]
 )
 
+AUTH_HEADER = { "WWW-Authenticate": "Bearer" }
+
+def http_exception(detail: str = "Invalid Token",
+                           status_code: int = status.HTTP_401_UNAUTHORIZED) -> HTTPException:
+    return HTTPException(
+        status_code=status_code,
+        detail=detail,
+        headers=AUTH_HEADER
+    )
+
 async def get_current_user(token: str = Depends(JwtBearer()),
                            db_session: AsyncConnection = Depends(get_database)):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Invalid Token",
-        headers={"WWW-Authenticate": "Bearer"}
-    )
-    
     try:
         payload = decode_access_token(token);
         username: str = payload.get("sub")
         if username is None:
-            raise credentials_exception
+            raise http_exception()
     except ExpiredSignatureError:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Token is expired",
-            headers={"WWW-Authenticate": "Bearer"}
-        )
+        raise http_exception(detail="Token is expired")
     except InvalidTokenError:
-        raise credentials_exception
+        raise http_exception()
     
     user = await UserRepository(db_session).get_by_email(username)
     if user is None:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="User does not exists",
-            headers={"WWW-Authenticate": "Bearer"}
-        )
+        raise http_exception(detail="User does not exists")
     return user
 
 @auth_router.post("/signin", response_model=TokenResponse, tags=["Token"])
@@ -53,11 +49,7 @@ async def sign_in(
     
     user = await UserRepository(db_session).authenticate(user_login)
     if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalud username or password",
-            headers={"WWW-Authenticate": "Bearer"}
-        )
+        raise http_exception(detail="Invalud username or password")
     expire_in_minutes = timedelta(minutes=TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
         data={"sub": user.email}, expires_delta=expire_in_minutes)
